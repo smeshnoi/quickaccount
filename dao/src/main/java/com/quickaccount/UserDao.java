cfc9befd212bcdc54e060d19f7ae400183c5e3cd
smeshnoi <sergey.zinkevich@gmail.by>
2018-02-13 17:10:17 +0300

    add userDaoTest

--- dao/src/main/java/com/quickaccount/UserDao.java
+++ dao/src/main/java/com/quickaccount/UserDao.java
@@ -1,6 +1,10 @@
 package com.quickaccount;
 
+import com.quickaccount.connection.ConnectionManager;
 import com.quickaccount.entity.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class UserDao extends BaseDao<User> {
     private static UserDao instance = null;
@@ -21,6 +25,19 @@ public class UserDao extends BaseDao<User> {
             }
         }
         return instance;
+    }
+
+    public User getUserByLogin(String login) {
+        Session session = ConnectionManager.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        User user = null;
+        Query<User> query = session.createQuery("select u from User u " +
+                "where u.login = :login ", User.class)
+                .setParameter("login", login);
+        user = query.list().get(0);
+        transaction.commit();
+        session.close();
+        return user;
     }
 
 }
--- dao/src/test/java/com/quickaccount/UserDaoTest.java
+++ dao/src/test/java/com/quickaccount/UserDaoTest.java
@@ -1,26 +1,51 @@
 package com.quickaccount;
 
+import com.quickaccount.connection.ConnectionManager;
 import com.quickaccount.entity.Contact;
 import com.quickaccount.entity.Currency;
 import com.quickaccount.entity.Role;
 import com.quickaccount.entity.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
 
 public class UserDaoTest {
     private SessionFactory sessionFactory;
 
-//    @Before
-//    public void initDb() {
-//        sessionFactory = new Configuration().configure().buildSessionFactory();
-//    }
+    @Before
+    public void initDb() {
+        ConnectionManager.openSessionFactory();
+    }
 
     @Test
-    public void getUserById() {
-                User user = new User("test","Test", "Testov",
-                new Currency("USD"), "passw", Role.USER,
+    public void getUserByLogin() {
+        Session session = ConnectionManager.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Currency currency = new Currency("USD");
+        CurrencyDao.getInstance().save(currency);
+        User user = new User("test","Test", "Testov",
+                currency, "passw", Role.USER,
                 new Contact("testA@gmail.com", "+375296465656"));
         UserDao.getInstance().save(user);
+        User user2 = new User("user","Test1", "Testov1",
+                currency, "passw", Role.USER,
+                new Contact("testuser@gmail.com", "+3752961111156"));
+        UserDao.getInstance().save(user2);
+        User test = UserDao.getInstance().getUserByLogin("test");
+        assertThat(UserDao.getInstance().getUserByLogin("test").getLogin(), equalTo("test"));
 
+        transaction.commit();
+        session.close();
+    }
+
+    @After
+    public void closeDB() {
+        ConnectionManager.getSessionFactory().close();
     }
 }
